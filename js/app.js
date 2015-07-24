aadf8febb9c2a3f6c8d1845cff83ba679029dcba
Christian Mosley <cmosley@beholdmedia.com>
2015-07-23 22:14:03 -0500

    hadouken complete

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
 
 	$(".ryu").mouseenter(function() {
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
-  });
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+ 	})
 
-	$(".ryu").mouseleave(function() {
-	$('.ryu-still').show();
-	$('.ryu-ready').hide();
-  });
+	.mouseleave(function() {
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+ 	})
+
+ 	.mousedown(function() {
+ 		playHadouken();
+  		$('.ryu-ready').hide();
+  		$('.ryu-throwing').show();
+  		$('.hadouken').finish().show()
+  			.animate({'left': '1020px'},
+			500, 
+			function() {
+				$(this).hide();
+				$(this).css('left', '525px');
+			});
+	})	
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});	
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = .5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
 
 
 
-});
