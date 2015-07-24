f7fde88c21be36189dd349b56631ce0fdebf9048
Christian Mosley <cmosley@beholdmedia.com>
2015-07-24 02:00:38 -0500

    final-2

--- js/app.js
+++ js/app.js
@@ -26,30 +26,27 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
+	});
+});
+		
 
-	$(document).keydown(function(e) {
-		if (e.which == 88) {
+$(document).keydown(function(e) {
+	if (e.which == 88) {
      		$('.ryu-ready').hide();
      		$('.ryu-throwing').hide();
      		$('.ryu-still').hide();
       		$('.ryu-cool').show();
       	}
-	});
+});
 
-	$(document).keyup(function(e) {
+$(document).keyup(function(e) {
 		if (e.which == 88) {
      		$('.ryu-ready').hide();
      		$('.ryu-throwing').hide();
-     		$('.ryu-still').show();
       		$('.ryu-cool').hide();
+      		$('.ryu-still').show();
 			}
-	});
-	
 });
-		
-
-
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
