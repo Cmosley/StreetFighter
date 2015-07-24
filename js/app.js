1ca3a8367898e69d9bfe05fd6dc694c61c1c703c
Christian Mosley <cmosley@beholdmedia.com>
2015-07-23 21:22:04 -0500

    mouseenter/mouseleave

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$(".ryu").mouseenter(function() {
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+  });
+
+	$(".ryu").mouseleave(function() {
+	$('.ryu-still').show();
+	$('.ryu-ready').hide();
+  });
+
+
+
+});
